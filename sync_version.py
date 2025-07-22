33e92f745bdf820d9c3f181ae28d7ec5d82e6da6
kaysen <xujiantao@letsflytech.com>
2025-07-22 17:43:37 +0800

    v0.1.6

--- sync_version.py
+++ sync_version.py
@@ -1,17 +1,17 @@
 import toml
 
-# 读取版本号文件
+# 读取 version.txt
 with open("version.txt", "r") as f:
     version = f.read().strip()
 
-# 更新 Cargo.toml
+# 更新 Cargo.toml 中的 [package].version
 cargo = toml.load("Cargo.toml")
 cargo['package']['version'] = version
 with open("Cargo.toml", "w") as f:
     toml.dump(cargo, f)
 
-# 更新 pyproject.toml
+# 更新 pyproject.toml 中的 [project].version
 pyproject = toml.load("pyproject.toml")
-pyproject['package']['version'] = version  # 这里改为访问 package
+pyproject['project']['version'] = version  # ✅ 改这里
 with open("pyproject.toml", "w") as f:
     toml.dump(pyproject, f)
