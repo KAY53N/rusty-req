0dabc2ada479ea3db1bfee52d4083d855a9b1fb5
KAY53N <kaysen820@gmail.com>
2025-09-26 13:39:11 +0800

    Update performance_test.py

--- tests/performance_test.py
+++ tests/performance_test.py
@@ -12,9 +12,9 @@ from concurrent.futures import ThreadPoolExecutor
 import os
 
 # Global settings
-GLOBAL_CONCURRENCY = 500
+GLOBAL_CONCURRENCY = 800
 TOTAL_TIMEOUT = 5.0  # Total timeout for all requests
-REQUEST_TIMEOUT = 4.0  # Timeout per request
+REQUEST_TIMEOUT = 4.5  # Timeout per request
 
 class PerformanceTest:
     def __init__(self):
